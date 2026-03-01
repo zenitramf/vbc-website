5ebdba2d53ac1ccd609fc9c9b9e320280afcefe8
zenitramf <francisco@zenitram.dev>
2026-02-28 17:04:49 -0800

    shadcn and tailwind install

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
 import { defineConfig } from "astro/config";
-
 import cloudflare from "@astrojs/cloudflare";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   adapter: cloudflare({
     imageService: "cloudflare",
-
     platformProxy: {
       enabled: true,
+    },
+    vite: {
+      plugins: [tailwindcss()],
     },
   }),
 });
