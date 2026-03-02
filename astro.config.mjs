2cc418457b429d3e1ee129b8733b0119ec99e811
zenitramf <francisco@zenitram.dev>
2026-03-02 06:56:46 -0800

    cf image passthru

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,8 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-	adapter: cloudflare({
-    imageService: "cloudflare",
+  adapter: cloudflare({
+    imageService: "passthrough",
     platformProxy: {
       enabled: true,
     },
